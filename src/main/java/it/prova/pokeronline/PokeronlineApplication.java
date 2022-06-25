399d1d353301a5e4bf7c9156c24bd635d3691043
cristianmarcelli <cristianma98@gmail.com>
2022-06-25 09:57:06 +0200

    Run effettuato con successo. Gli utenti accedono al sistema generando un token

--- src/main/java/it/prova/pokeronline/PokeronlineApplication.java
+++ src/main/java/it/prova/pokeronline/PokeronlineApplication.java
@@ -1,13 +1,74 @@
 package it.prova.pokeronline;
 
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import it.prova.pokeronline.model.Ruolo;
+import it.prova.pokeronline.model.Utente;
+import it.prova.pokeronline.service.RuoloService;
+import it.prova.pokeronline.service.UtenteService;
+
 @SpringBootApplication
-public class PokeronlineApplication {
+public class PokeronlineApplication implements CommandLineRunner {
+	
+	@Autowired
+	private RuoloService ruoloServiceInstance;
+	@Autowired
+	private UtenteService utenteServiceInstance;
 
 	public static void main(String[] args) {
 		SpringApplication.run(PokeronlineApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		if (ruoloServiceInstance.cercaPerDescrizioneECodice("Administrator", Ruolo.ROLE_ADMIN) == null) {
+			ruoloServiceInstance.inserisciNuovo(new Ruolo("Administrator", Ruolo.ROLE_ADMIN));
+		}
+
+		if (ruoloServiceInstance.cercaPerDescrizioneECodice("Special Player", Ruolo.ROLE_SPECIAL_PLAYER) == null) {
+			ruoloServiceInstance.inserisciNuovo(new Ruolo("Special Player", Ruolo.ROLE_SPECIAL_PLAYER));
+		}
+		
+		if (ruoloServiceInstance.cercaPerDescrizioneECodice("Classic Player", Ruolo.ROLE_PLAYER) == null) {
+			ruoloServiceInstance.inserisciNuovo(new Ruolo("Classic Player", Ruolo.ROLE_PLAYER));
+		}
+
+		// a differenza degli altri progetti cerco solo per username perche' se vado
+		// anche per password ogni volta ne inserisce uno nuovo, inoltre l'encode della
+		// password non lo
+		// faccio qui perche gia lo fa il service di utente, durante inserisciNuovo
+		if (utenteServiceInstance.findByUsername("admin") == null) {
+			Utente admin = new Utente("admin", "admin", "Mario", "Rossi", new Date());
+			admin.getRuoli().add(ruoloServiceInstance.cercaPerDescrizioneECodice("Administrator", Ruolo.ROLE_ADMIN));
+			utenteServiceInstance.inserisciNuovo(admin);
+			// l'inserimento avviene come created ma io voglio attivarlo
+			utenteServiceInstance.changeUserAbilitation(admin.getId());
+		}
+
+		if (utenteServiceInstance.findByUsername("user") == null) {
+			Utente classicUser = new Utente("user", "user", "Antonio", "Verdi", new Date());
+			classicUser.getRuoli()
+					.add(ruoloServiceInstance.cercaPerDescrizioneECodice("Special Player", Ruolo.ROLE_SPECIAL_PLAYER));
+			utenteServiceInstance.inserisciNuovo(classicUser);
+			// l'inserimento avviene come created ma io voglio attivarlo
+			utenteServiceInstance.changeUserAbilitation(classicUser.getId());
+		}
+
+		if (utenteServiceInstance.findByUsername("user1") == null) {
+			Utente classicUser1 = new Utente("user1", "user1", "Antonioo", "Verdii", new Date());
+			classicUser1.getRuoli()
+					.add(ruoloServiceInstance.cercaPerDescrizioneECodice("Classic Player", Ruolo.ROLE_PLAYER));
+			utenteServiceInstance.inserisciNuovo(classicUser1);
+			// l'inserimento avviene come created ma io voglio attivarlo
+			utenteServiceInstance.changeUserAbilitation(classicUser1.getId());
+		}
+
 	}
 
 }
